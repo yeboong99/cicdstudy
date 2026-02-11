2df15365e7843c7457fa15bb340d91a5e75fd1ba
yeboong99 <yechani9905@gmail.com>
2026-02-12 01:40:19 +0900

    [Start] Project created

---
+++ src/main/java/com/neulboong/forcicd/ForcicdApplication.java
@@ -0,0 +1,13 @@
+package com.neulboong.forcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForcicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neulboong/forcicd/ForcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neulboong.forcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
